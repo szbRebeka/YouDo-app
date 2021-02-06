5d69eea610c40479c27f4d8f0b81e8abbb80fdaa
szbRebeka <szrebeka94@gmail.com>
2021-02-06 16:46:45 +0100

    mz-06 left unfinished

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,53 +1,54 @@
-//can be deleted
-console.log("Console log works!");
-//variables for the form
 let youDoFrom = document.querySelector(".youDo-form");
-//let createBtn = document.querySelector(".create-btn");
 
 //creating ul for future items
 let youDoList = document.createElement("ul");
 document.body.appendChild(youDoList);
 youDoList.classList.add("todos");
 
-//list item template
-let newToDoText = document.querySelector("#write-todo");
-newToDoText.classList.add("todo");
-//let toDoTemplate = `<li> <span>${newToDoText.value}</span><button>delete</button></li>`;
 
-//function the new todo item
-
+//function to create template
 let addToDoTemplate = () => {
-    let todoContent = `<li><span>${youDoFrom.newinput.value}</span><button class="delete">delete</button></li>`;
-    youDoList.innerHTML += todoContent
+    let todoContent = `<li><span class="todoText">${youDoFrom.newinput.value}</span><button class="delete">delete</button></li>`;
+    youDoList.innerHTML += todoContent;
+    //saving to local storage
+    localStorage.setItem("task", youDoFrom.newinput.value);
 };
 
 //add new items to ul
 youDoFrom.addEventListener("submit", e => {
-    console.log(youDoFrom.newinput.value); //delete later
     e.preventDefault();
     addToDoTemplate();
-    //listFrame.append(newToDoText.value)might use later
     youDoFrom.reset();
 });
 
 //deleting list items
 youDoList.addEventListener("click", event => {
     if (event.target.classList.contains("delete")) {
-        event.target.parentElement.remove()
+        event.target.parentElement.remove();
     }
 });
 // Validating the input
-
 youDoFrom.newinput.addEventListener("keyup", e => {
     if (e.target.value.length >= 6) {
         e.target.classList.add("input-valid");
         e.target.classList.remove("input-non-valid");
         //Restricting submissions
         youDoFrom.submit.disabled = false;
+    } else if (e.target.value.length === 0) {
+        e.target.classList.remove("input-valid");
     } else {
         e.target.classList.add("input-non-valid");
         e.target.classList.remove("input-valid");
     }
 });
+
+
+
+
+
+
+
+
+
 
 
