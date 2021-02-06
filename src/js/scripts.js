db75a8428cca300af536569a166c742b40180136
szbRebeka <szrebeka94@gmail.com>
2021-02-06 12:38:44 +0100

    finish mz-03

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,21 +2,39 @@
 console.log("Console log works!");
 //variables for the form
 let youDoFrom = document.querySelector(".youDo-form");
-let createBtn = document.querySelector(".create-btn");
+//let createBtn = document.querySelector(".create-btn");
 
 //creating ul for future items
 let listFrame = document.createElement("ul");
 document.body.appendChild(listFrame);
 listFrame.classList.add("todos");
 
+//list item template
+let newToDoText = document.querySelector("#write-todo");
+newToDoText.classList.add("todo");
+//let toDoTemplate = `<li> <span>${newToDoText.value}</span><button>delete</button></li>`;
+
+//function the new todo item
+
+let addToDoTemplate = () => {
+    let todoContent = `<li><span>${youDoFrom.new.value}</span><button class="delete">delete</button></li>`;
+    listFrame.innerHTML += todoContent
+};
+
 //add new items to ul
-createBtn.addEventListener("click", () => {
-    let newToDoText = document.querySelector("#write-todo");
-    console.log(newToDoText.value); //delete later
-    newToDoText.classList.add("todo");
-    listFrame.innerHTML += `<li>${newToDoText.value}</li>`
-    //listFrame.append(newToDoText.value) might use later
+youDoFrom.addEventListener("submit", e => {
+    console.log(youDoFrom.new.value); //delete later
+    e.preventDefault();
+    addToDoTemplate();
+    //listFrame.append(newToDoText.value)might use later
     youDoFrom.reset();
+});
+
+//deleting list items
+listFrame.addEventListener("click", event => {
+    if (event.target.classList.contains("delete")) {
+        event.target.parentElement.remove()
+    }
 });
 
 
