5348f51b37c594b0663c0acb8cfbd003e0ca6d65
szbRebeka <szrebeka94@gmail.com>
2021-02-06 10:00:21 +0100

    finish mz-01

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,2 +1,12 @@
 //can be deleted
 console.log("Console log works!");
+let youDoFrom = document.querySelector(".youDo-form");
+let createBtn = document.querySelector(".create-btn");
+
+createBtn.addEventListener("click", () => {
+    let newToDoText = document.querySelector("#write-todo");
+    console.log(newToDoText.value);
+    youDoFrom.reset();
+})
+   // newToDo.classList.add("newToDoStyle");
+   // ul.append(newToDo);
