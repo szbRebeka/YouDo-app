6570911e33c4a73848bfda24d09ac1903517ca03
szbRebeka <szrebeka94@gmail.com>
2021-02-06 10:31:05 +0100

    finish mz-02

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,22 @@
 //can be deleted
 console.log("Console log works!");
+//variables for the form
 let youDoFrom = document.querySelector(".youDo-form");
 let createBtn = document.querySelector(".create-btn");
 
+//creating ul for future items
+let listFrame = document.createElement("ul");
+document.body.appendChild(listFrame);
+listFrame.classList.add("todos");
+
+//add new items to ul
 createBtn.addEventListener("click", () => {
     let newToDoText = document.querySelector("#write-todo");
-    console.log(newToDoText.value);
+    console.log(newToDoText.value); //delete later
+    newToDoText.classList.add("todo");
+    listFrame.innerHTML += `<li>${newToDoText.value}</li>`
+    //listFrame.append(newToDoText.value) might use later
     youDoFrom.reset();
-})
-   // newToDo.classList.add("newToDoStyle");
-   // ul.append(newToDo);
+});
+
+
